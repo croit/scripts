31fa10c4e91d3828c4afe0912142ea96d9543bef
Server <server@croit.io>
2017-09-14 11:58:44 +0200

    backup to tgz files to prevent file perm. issues
    After some testing, we saw a problem with file permissions especial with external storage.
    To work around this problem, we decided to store the backup in a tar archive.
    In addition we now save the container release tags per backup for recovery.

--- config.backup.sh
+++ config.backup.sh
@@ -5,17 +5,42 @@
 # This software may be modified and distributed under the terms
 # of the MIT license.  See the LICENSE file for details.
 
-# adjust backup dir as needed
-BACKUP_DIR=/backups/croit/$(date +%s)
+# verify that the croit container exists
+CONTAINER=$(docker container list -a --format '{{.Names}}' --no-trunc | egrep "^croit$")
+if [[ -z ${CONTAINER} ]]
+then
+    # container does not exists
+    echo "Please make sure that a docker container named 'croit' exists."
+    exit 1
+fi
 
+# adjust backup dir as needed
+BACKUP_DIR="/backups/croit/$(date +%s)"
+
+echo "Starting backup to ${BACKUP_DIR}"
+mkdir -p "${BACKUP_DIR}/"
+
+# we need to gather the directorys while the docker container runs
 MYSQL_DATA=$(docker inspect --format='{{range .Mounts}}{{if (eq .Destination "/var/lib/mysql")}}{{.Source}}{{end}}{{end}}' croit)
 CONFIG_DATA=$(docker inspect --format='{{range .Mounts}}{{if (eq .Destination "/config")}}{{.Source}}{{end}}{{end}}' croit)
+RELEASE=$(docker inspect --format='{{.Config.Image}}' croit)
 
+# Shutdown container befor creating backup
 docker stop croit
-mkdir -p "${BACKUP_DIR}/config"
-mkdir -p "${BACKUP_DIR}/db"
-cp -r "${MYSQL_DATA}" "${BACKUP_DIR}/db"
-cp -r "${CONFIG_DATA}" "${BACKUP_DIR}/config"
+
+# Database backup
+echo "create database backup from ${MYSQL_DATA}"
+[[ -z ${MYSQL_DATA} ]] && {echo "No MySQL/MariaDB data directory found"} || tar czf "${BACKUP_DIR}/db.tgz" -C "${MYSQL_DATA}" .
+
+# configuration backup
+echo "create config backup from ${MYSQL_DATA}"
+[[ -z ${CONFIG_DATA} ]] && {echo "No croit config data directory found"} || tar czf "${BACKUP_DIR}/config.tgz" -C "${CONFIG_DATA}" .
+
+# save the container release tag
+echo "backup based on a container with tags ${RELEASE}"
+[[ -z ${RELEASE} ]] && {echo "Unable to detect the container release tags"} || echo ${RELEASE} > "${BACKUP_DIR}/release"
+
+# Start the container again
 docker start croit
 
 
--- config.restore.sh
+++ config.restore.sh
@@ -7,7 +7,8 @@
 
 # adjust backup dir as needed
 BACKUP_DIR="$(ls -v1d /backups/croit/* |tail -n 1)" # gets the latest backup directory
-CROIT_RELEASE="croit/croit:latest"
+CROIT_RELEASE="$(cat ${BACKUP_DIR}/release)"
+[[ -z ${CROIT_RELEASE} ]] && CROIT_RELEASE="croit:latest"
 
 # verify no croit container exists
 CONTAINERS=$(docker container list -a --format '{{.Names}}' --no-trunc | egrep "^(croit|croit-data)$")
@@ -18,22 +19,33 @@ then
 	exit 1
 fi
 # Verify BACKUP_DIR exists
-if [[ ! -d "${BACKUP_DIR}" ]] || [[ ! -d "${BACKUP_DIR}/config" ]] || [[ ! -d "${BACKUP_DIR}/db" ]]
+if [[ ! -d "${BACKUP_DIR}" ]] || [[ ! -f "${BACKUP_DIR}/config.tgz" ]] || [[ ! -f "${BACKUP_DIR}/db.tgz" ]]
 then
 	# Backup directory contains no data
 	echo "Your backup directory does not contain data. Please make sure to set the BACKUP_DIR variable."
 	exit 1
 fi
 
+echo "Restoring Backup from ${BACKUP_DIR}"
+echo "with container release version ${CROIT_RELEASE}"
+
 # Create a docker data container
 docker pull ${CROIT_RELEASE}
 docker create --name croit-data ${CROIT_RELEASE}
 
+# Database restore
 MYSQL_DATA=$(docker inspect --format='{{range .Mounts}}{{if (eq .Destination "/var/lib/mysql")}}{{.Source}}{{end}}{{end}}' croit-data)
-CONFIG_DATA=$(docker inspect --format='{{range .Mounts}}{{if (eq .Destination "/config")}}{{.Source}}{{end}}{{end}}' croit-data)
+echo "Restoring MySQL/MariaDB"
+echo "Source      = ${BACKUP_DIR}/db.tgz"
+echo "Destination = ${MYSQL_DATA}"
+[[ -z ${MYSQL_DATA}  ]] && {echo "No MySQL/MariaDB data directory found"} || tar xf "${BACKUP_DIR}/db.tgz" -C "${MYSQL_DATA}"
 
-[[ -z ${MYSQL_DATA}  ]] && {echo "No MySQL data directory found"} || rsync -avp --delete "${BACKUP_DIR}/db" "${MYSQL_DATA}"
-[[ -z ${CONFIG_DATA} ]] && {echo "No config data directory found"} || rsync -avp --delete "${BACKUP_DIR}/config" "${CONFIG_DATA}"
+# Configuration restore
+CONFIG_DATA=$(docker inspect --format='{{range .Mounts}}{{if (eq .Destination "/config")}}{{.Source}}{{end}}{{end}}' croit-data)
+echo "Restoring croit config"
+echo "Source      = ${BACKUP_DIR}/config.tgz"
+echo "Destination = ${CONFIG_DATA}"
+[[ -z ${CONFIG_DATA} ]] && {echo "No config data directory found"} || tar xf "${BACKUP_DIR}/config.tgz" -C "${CONFIG_DATA}"
 
 echo "backup restored to data container 'croit-data', starting container croit now"
 docker run --net=host --restart=always --volumes-from croit-data --name croit -d ${CROIT_RELEASE}
